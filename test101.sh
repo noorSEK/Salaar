91a0e91d1a28f726f95962cc6b8d1bd33e8cea03
marqueea/href=https://evil.comPLEASE_DONT_CLICK_ME-I-WILL/a <148883048+noorSEK@users.noreply.github.com>
2025-02-10 21:27:24 +0530

    Update test101.sh

--- test101.sh
+++ test101.sh
@@ -54,7 +54,7 @@ with open("bug_bounty_domains.txt", "w") as f:
 EOF
 
 echo "Extraction complete! Check 'bug_bounty_domains.txt'"
-
+mkdir /opt/
 
 echo "==================================="
 echo "💀 Salaar Bug Bounty Automation 💀"
@@ -68,8 +68,89 @@ cat bug_bounty_domains.txt | while read domain; do curl -s "https://crt.sh/?q=%.
 cat bug_bounty_domains.txt | chaos -silent -key >> subs-temp.txt
 
 # Merge and sort unique subdomains
-cat amass-subs.txt subs-temp.txt | sort -u | tee subdomains.txt
+cat amass-subs.txt subs-temp.txt | sort -u | shuf | tee subdomains.txt
+rm amass-subs.txt subs-temp.txt
 
 # Filtering out Dead Domains
 subs-temp.txt | httpx | tee live-subdomains.txt 
+
+
+katana
+waybackurls
+hackrawler
+gau
+gauplus
+urlfinder
+
+
+# Crawl URLs and extract parameters & JS files
+cat subdomains.txt | katana -d 5 >> /opt/katana-urls.txt
+cat /opt/katana-urls.txt | grep = | qsreplace salaar >> /opt/katana-params.txt
+cat /opt/katana-urls.txt | grep .js >> /opt/katana-js-files.txt
+cat /opt/katana-urls.txt | grep -Ei "token=|key=|apikey=|access_token=|secret=|auth=|password=|session=|jwt=|bearer=|Authorization=|Bearer |eyJ|AWS_ACCESS_KEY_ID|AWS_SECRET_ACCESS_KEY" >> /opt/katana-kong.txt
+cat /opt/katana-urls.txt | grep -Ei "wp-content|wp-login|wp-admin|wp-includes|wp-json|xmlrpc.php|wordpress|wp-config|wp-cron.php" >> /opt/katana-wordpress.txt
+rm /opt/katana-urls.txt
+
+cat subdomains.txt | urlfinder >> /opt/urlfinder-urls.txt
+cat /opt/urlfinder-urls.txt | grep = | qsreplace salaar >> /opt/urlfinder-params.txt
+cat /opt/urlfinder-urls.txt | grep .js >> /opt/urlfinder-js-files.txt
+cat /opt/urlfinder-urls.txt | grep -Ei "token=|key=|apikey=|access_token=|secret=|auth=|password=|session=|jwt=|bearer=|Authorization=|Bearer |eyJ|AWS_ACCESS_KEY_ID|AWS_SECRET_ACCESS_KEY" >> /opt/urlfinder-kong.txt
+cat /opt/urlfinder-urls.txt | grep -Ei "wp-content|wp-login|wp-admin|wp-includes|wp-json|xmlrpc.php|wordpress|wp-config|wp-cron.php" >> /opt/urlfinder-wordpress.txt
+rm /opt/urlfinder-urls.txt
+
+#cat /opt/katana-params.txt /opt/urlfinder-params.txt | sort -u >> /opt/help1-params.txt 
+#rm  /opt/katana-params.txt /opt/urlfinder-params.txt
+
+#cat /opt/katana-js-files.txt /opt/urlfinder-js-files.txt | sort -u >> /opt/help1-js-files.txt
+#rm  /opt/katana-js-files.txt /opt/urlfinder-js-files.txt
+
+#cat /opt/katana-kong.txt /opt/urlfinder-kong.txt | sort -u >> /opt/help1-js-files.txt
+
+cat live-subdomains.txt | hakrawler -u -i -insecure >> /opt/hakrawler-urls.txt
+cat /opt/hakrawler-urls.txt | grep = | qsreplace salaar >> /opt/hakrawler-params.txt
+cat /opt/hakrawler-urls.txt | grep .js >> /opt/hakrawler-js-files.txt
+cat /opt/hakrawler-urls.txt | grep -Ei "token=|key=|apikey=|access_token=|secret=|auth=|password=|session=|jwt=|bearer=|Authorization=|Bearer |eyJ|AWS_ACCESS_KEY_ID|AWS_SECRET_ACCESS_KEY" >> /opt/hakrawler-kong.txt
+cat /opt/hakrawler-urls.txt | grep -Ei "wp-content|wp-login|wp-admin|wp-includes|wp-json|xmlrpc.php|wordpress|wp-config|wp-cron.php" >> /opt/hakrawler-wordpress.txt
+rm /opt/hakrawler-urls.txt
+
+for domain in $(cat subdomains.txt); do  
+    curl -s "http://web.archive.org/cdx/search/cdx?url=*.$domain/*&output=text&fl=original" | sort -u >> /opt/wayback-urls.txt  
+    sleep $((RANDOM % 5 + 3))  # Random delay (3-7 seconds)
+done
+
+cat /opt/wayback-urls.txt   | grep = | qsreplace salaar >> /opt/way-params.txt
+cat /opt/wayback-urls.txt   | grep .js >> /opt/way-js-files.txt
+cat /opt/wayback-urls.txt   | grep -Ei "token=|key=|apikey=|access_token=|secret=|auth=|password=|session=|jwt=|bearer=|Authorization=|Bearer |eyJ|AWS_ACCESS_KEY_ID|AWS_SECRET_ACCESS_KEY" >> /opt/way-kong.txt
+cat /opt/wayback-urls.txt   | grep -Ei "wp-content|wp-login|wp-admin|wp-includes|wp-json|xmlrpc.php|wordpress|wp-config|wp-cron.php" >> /opt/way-wordpress.txt
+rm  /opt/wayback-urls.txt
+
+cat live-subdomains.txt | gau >> /opt/hakrawler-urls.txt
+cat /opt/gau-urls.txt   | grep = | qsreplace salaar >> /opt/gau-params.txt
+cat /opt/gau-urls.txt   | grep .js >> /opt/gau-js-files.txt
+cat /opt/gau-urls.txt   | grep -Ei "token=|key=|apikey=|access_token=|secret=|auth=|password=|session=|jwt=|bearer=|Authorization=|Bearer |eyJ|AWS_ACCESS_KEY_ID|AWS_SECRET_ACCESS_KEY" >> /opt/gau-kong.txt
+cat /opt/gau-urls.txt   | grep -Ei "wp-content|wp-login|wp-admin|wp-includes|wp-json|xmlrpc.php|wordpress|wp-config|wp-cron.php" >> /opt/gau-wordpress.txt
+rm /opt/gau-urls.txt
+
+# Separating js Files, PArameters 
+cat /opt/katana-params.txt /opt/urlfinder-params.txt /opt/hakrawler-params.txt /opt/way-params.txt /opt/gau-params.txt | sort -u >> params.txt
+rm /opt/katana-params.txt /opt/urlfinder-params.txt /opt/hakrawler-params.txt /opt/way-params.txt /opt/gau-params.txt
+
+cat /opt/katana-js-files.txt /opt/urlfinder-params.txt /opt/hakrawler-params.txt /opt/way-js-files.txt /opt/gau-js-files.txt | sort -u >> js-files.txt
+rm /opt/katana-js-files.txt /opt/urlfinder-params.txt /opt/hakrawler-params.txt /opt/way-js-files.txt /opt/gau-js-files.txt
+
+cat /opt/katana-kong.txt /opt/urlfinder-kong.txt /opt/hakrawler-kong.txt /opt/way-kong.txt /opt/gau-kong.txt | sort -u >> key-urls.txt
+rm /opt/katana-kong.txt /opt/urlfinder-kong.txt /opt/hakrawler-kong.txt /opt/way-kong.txt /opt/gau-kong.txt
+
+cat /opt/katana-wordpress.txt /opt/urlfinder-wordpress.txt /opt/hakrawler-wordpress.txt /opt/way-wordpress.txt /opt/gau-wordpress.txt | sort -u >> wordpress-urls.txt
+rm /opt/katana-wordpress.txt /opt/urlfinder-wordpress.txt /opt/hakrawler-wordpress.txt /opt/way-wordpress.txt /opt/gau-wordpress.txt 
+
+
+# Fuzzing Js Files 
+cat js-files.txt | nuclei -t /root/nuclei-templates/http/exposures/ -nh >> nuclei-js-results.txt
+cat js-files.txt | mantra >> js-mantra-results.txt
+
+# Fuzzing Params 
+cat params.txt | qsreplace '<u>hyper</u>' | while read host do ; do curl --silent --path-as-is -L --insecure "$host" | grep -qs "<u>hyper" && echo "$host"; done | tee htmli.txt 
+
+
 
